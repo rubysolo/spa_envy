ac0870c60edbdd5263fa075e6561776bc3457159
Solomon White <rubysolo@gmail.com>
2018-01-11 23:13:03 -0700

    implement blackholing for SPA deep links

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"log"
@@ -9,6 +10,7 @@ import (
 	"strings"
 )
 
+// return process environment formatted as javascript object
 func envMap() map[string]string {
 	env := make(map[string]string)
 	for _, pair := range os.Environ() {
@@ -28,6 +30,46 @@ func makeEnvHandler(envJSON []byte) http.HandlerFunc {
 	}
 }
 
+// wrapped http.FileServer that can serve index.html for any
+// 404 (to support SPA deep-links)
+type StaticHandler struct {
+	fileServer http.Handler
+}
+
+type response struct {
+	buffer bytes.Buffer
+	status int
+	header http.Header
+}
+
+func (w *response) Write(p []byte) (int, error) {
+	return w.buffer.Write(p)
+}
+func (w *response) WriteHeader(status int) {
+	w.status = status
+}
+func (w *response) Header() http.Header {
+	return w.header
+}
+
+func (h *StaticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	wrapped := &response{header: w.Header()}
+
+	// pass request to native file server
+	h.fileServer.ServeHTTP(wrapped, r)
+
+	// intercept 404
+	if wrapped.status == http.StatusNotFound {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(200)
+		http.ServeFile(w, r, "static/index.html")
+		return
+	}
+
+	w.WriteHeader(wrapped.status)
+	io.Copy(w, &wrapped.buffer)
+}
+
 func main() {
 	envJSON, err := json.MarshalIndent(envMap(), "", "  ")
 	if err != nil {
@@ -35,7 +77,7 @@ func main() {
 	}
 	http.HandleFunc("/env.js", makeEnvHandler(envJSON))
 
-	fs := http.FileServer(http.Dir("static"))
+	fs := &StaticHandler{fileServer: http.FileServer(http.Dir("static"))}
 	http.Handle("/", fs)
 
 	log.Println("Listening...")
